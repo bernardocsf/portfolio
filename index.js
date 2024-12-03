c42d64399c57cbeffe4bee11ec6a4155ba469c18
Bernardo Ferreira <bernardocoelhoferreira@gmail.com>
2024-12-03 22:51:42 +0000

    lastchance

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ app.post("/send-email", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(new URL(".", import.meta.url).pathname, "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(new URL(".", import.meta.url).pathname, "dist", "index.html"));
 });
 
 
