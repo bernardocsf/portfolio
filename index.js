94bde9dfbbfa124b6f11e6cbdf5cfb9f5dcc39fd
Bernardo Ferreira <bernardocoelhoferreira@gmail.com>
2024-12-03 22:45:59 +0000

    ok

--- index.js
+++ index.js
@@ -36,5 +36,12 @@ app.post("/send-email", async (req, res) => {
   }
 });
 
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
